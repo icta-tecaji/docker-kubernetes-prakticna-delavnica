b7e58d42657e49e89d80a2c6f7339021a9280e8f
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2024-11-20 14:08:32 +0100

    fix helpers

--- helpers/restore.sh
+++ helpers/restore.sh
@@ -5,11 +5,11 @@
 docker system prune -a
 docker rmi $(docker images -a -q)
 
-#sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-compose-plugin
-#sudo rm -rf /var/lib/docker
-#sudo rm -rf /var/lib/containerd
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
 
-#sudo rm -rf docker-k8s-public/
+sudo rm -rf docker-kubernetes-prakticna-delavnica
 
 sudo apt update
 sudo apt -y upgrade
@@ -17,3 +17,5 @@ sudo apt -y dist-upgrade
 
 sudo apt-get purge nginx nginx-common
 sudo apt-get autoremove
+
+sudo ufw status
